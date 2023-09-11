8bc942c165dff9fcc6da1713c4a65b8515e46328
sunghyuki <sunghyuk1609@gmail.com>
2023-09-11 10:56:15 +0900

    feat: checkedException -> uncheckedException(인터페이스와 서비스 계층의 순수성을 유지)

---
+++ src/main/java/hello/jdbc/connection/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello.jdbc.connection.repository;
+
+import hello.jdbc.connection.domain.Member;
+
+public interface MemberRepository {
+    Member save(Member member);
+
+    Member findById(String memberId);
+
+    void update(String memberId, int money);
+
+    void delete(String memberId);
+}
